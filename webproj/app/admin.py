e2c1ce44e28e334611c1326dda44dfddddb1037c
alex-pt01 <aarodrigues@ua.pt>
2021-04-13 10:55:36 +0100

    entities added

--- webproj/app/admin.py
+++ webproj/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import *
+
+admin.site.register(Promotion)
+admin.site.register(Product)
--- webproj/app/models.py
+++ webproj/app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Promotion(models.Model):
+    name = models.CharField(max_length=80)
+    discount = models.FloatField(null=True)
+    description = models.CharField(max_length=300)
+    deadline = models.DateField()
+
+class Product(models.Model):
+    name = models.CharField(max_length=80)
+    price = models.FloatField(null=True)
+    description = models.CharField(max_length=300)
+    ranking = models.IntegerField()
+    image = models.ImageField(upload_to='static/images')
+    quantity = models.IntegerField()
+    stock = models.BooleanField()
+    brand = models.CharField(max_length=80)
+    CATEGORY = (('Smartphones','Smartphones'),
+                ('Computers','Computers'),
+                ('Tablets','Tablets'),
+                ('Drones','Drones')
+                ,('Televisions','Televisions'))
+    category = models.CharField(max_length= 150, choices = CATEGORY)
+    promotion = models.ForeignKey(Promotion, on_delete=models.CASCADE)
+
+
